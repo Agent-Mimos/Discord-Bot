57126cf13a0c1a2cc2bc45791d74cc74ef547347
Agent-Mimos <mihaigifei0@gmail.com>
2020-09-07 20:11:54 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -16,5 +16,5 @@ module.exports.run = async (bot, message, args) => {
 module.exports.config = {
     name: "ping",
     aliases: ["latency"],
-    description: 'Latency of the bot'
+    description: ''
 }
